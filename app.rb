ed0e192d8dadaa8ae370d9bcd4e3932742b61020
Yuki-Morikawa <myrukuwi@gmail.com>
2019-04-19 06:54:42 +0000

    first commit

--- app.rb
+++ app.rb
@@ -7,8 +7,9 @@ require 'net/http'
 require 'json'
 
 get '/' do 
+    keyword = params[:keyword]
     uri = URI("https://itunes.apple.com/search")
-    uri.query = URI.encode_www_form({ term: "Maroon 5", country: "US", media: "music", limit: 10})
+    uri.query = URI.encode_www_form({ term: "keyword", country: "US", media: "music", limit: 10})
     res = Net::HTTP.get_response(uri)
     returned_json = JSON.parse(res.body)
     @music = returned_json["results"]
