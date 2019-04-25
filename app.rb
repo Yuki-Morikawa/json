fc8d16c890d8b539b0d3d737de3f1aa98c815e49
Yuki-Morikawa <myrukuwi@gmail.com>
2019-04-25 17:41:25 +0000

    app.rb

--- app.rb
+++ app.rb
@@ -8,6 +8,21 @@ require 'json'
 require './models'
 
 get '/' do 
+    keyword = params[:keyword]
+    uri = URI.params("https://itunes.apple.com/search")
+    uri.query = URI.encode_www_form({ term: keyword, country: "US", media: "music", limit: 10})
+    res = Net::HTTP.get_response(uri)
+    returned_json = JSON.parse(res.body)
+
+    returned_json["results"].map! do |music|
+        music_info = Music.find_or_create_by(track_id: music["trackId"])
+        music["id"] = music_info.id
+        music["like"] = music_info.like
+        music 
+    end
+
+    @music = returned_json["results"]
+
     erb :index
 end
 
@@ -27,10 +42,6 @@ get '/search' do
 
     json returned_json["results"]
 
-
-@music = returned_json["results"]
-
-erb :index
 end
 
 
