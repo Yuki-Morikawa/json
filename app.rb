7d60eb995c3dc736fe616856f3739c54ae3a88de
Yuki-Morikawa <myrukuwi@gmail.com>
2019-04-26 09:45:29 +0000

    jQuery-code

--- app.rb
+++ app.rb
@@ -8,9 +8,12 @@ require 'json'
 require './models'
 
 get '/' do 
-
-    
-   
+    keyword = params[:keyword]
+    uri = URI("https://itunes.apple.com/search")
+    uri.query = URI.encode_www_form({ term: keyword, country: "US", media: "music", limit: 10})
+    res = Net::HTTP.get_response(uri)
+    returned_json = JSON.parse(res.body)
+    @music = returned_json["results"]
    
 
     erb :index
