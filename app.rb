200c7c58b82e04ffcd2d4aa1fdd3963c7b3674ec
Yuki-Morikawa <myrukuwi@gmail.com>
2019-04-26 09:35:00 +0000

    jQuery-code

--- app.rb
+++ app.rb
@@ -10,21 +10,7 @@ require './models'
 get '/' do 
 
     
-    keyword = params[:keyword]
-    uri = URI.parse("https://itunes.apple.com/search")
-    uri.query = URI.encode_www_form({ term: keyword, country: "US", media: "music", limit: 10})
-    res = Net::HTTP.get_response(uri)
-    returned_json = JSON.parse(res.body)
-
-    returned_json["results"].map! do |music|
-        music_info = Music.find_or_create_by(track_id: music["trackId"])
-        music["id"] = music_info.id
-        music["like"] = music_info.like
-        music 
-    end
-
-    @music = returned_json["results"]
-
+   
    
 
     erb :index
